0092b24978d0215027648360d1277fd5b0dc0ea2
alexlearning77 <alex_learning@hotmail.com>
2022-01-04 01:50:49 +0100

    Sorting Js

--- main.js
+++ main.js
@@ -2,4 +2,23 @@ function computerPlay (max) {
     return Math.floor(Math.random() * max);
 }
 
+let score = computerPlay(3);
+let result;
+
+function outcome (score) {
+    if (score == 0) {
+        result = "Rock!";
+    } else if (score == 1) {
+        result = "Paper!";
+    } else {
+        result = "Scissors!";
+    }
+    return result;
+}
+console.log(outcome(score));
+ 
+// function displayScore () {
+//     let output = document.getElementById('result');
+//     output.innerHTML = `Result is: ' + ${outcome(score)};
+// }
 
