49d394b2dafadbf7c666da7414d023ca998ab58c
alexlearning77 <alex_learning@hotmail.com>
2022-01-04 02:07:07 +0100

    Displaying "computer" result on page

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function computerPlay (max) {
 let score = computerPlay(3);
 let result;
 
-function outcome (score) {
+function outcome(score) {
     if (score == 0) {
         result = "Rock!";
     } else if (score == 1) {
@@ -15,10 +15,11 @@ function outcome (score) {
     }
     return result;
 }
-console.log(outcome(score));
+// console.log(outcome(score));
  
-// function displayScore () {
-//     let output = document.getElementById('result');
-//     output.innerHTML = `Result is: ' + ${outcome(score)};
-// }
-
+function displayScore () {
+    let output = document.getElementById('result');
+    output.innerHTML = "Result is: " + outcome(score);
+}
+ 
+console.log(displayScore());
